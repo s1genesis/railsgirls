cb64dfcc0600300ca423fad87e11efd9f57e4182
Michael Morales <morales_michael@fastmail.fm>
2014-01-18 17:38:31 -0700

    Adding picture uploads

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'b15b6fa3a45c5eade565c902a8f653d358e7987a1153931954cb6a7a153f14ebbdede43ab7641c5b15f4f138f2cd26b8404ff5bdc2bf2fbf3111004dc76bceb8'
